8bbc785c0ee38b3a2d0f2db5d0348cdbab25b28f
Jon He <daddy@192.168.1.100>
2024-05-12 12:05:45 +0100

    modified main

--- main.py
+++ main.py
@@ -1,15 +1,52 @@
 import random
+import os
+import json
+
+def error_of_tolerance (true_answer, user_answer):
+    difference = 0
+    pre_check = len(true_answer) - len(user_answer)
+    if pre_check > 0:
+        difference += pre_check
+    if pre_check < 0:
+        pass
+    i = 0
+    while i < len(true_answer) and i < len(user_answer):
+        if true_answer[i] != user_answer[i]:
+            difference += 1
+        i += 1
+    return (difference/len(true_answer))*100
+
+
+file_path = 'biology.json'
 print('Flashcard Program')
 flash_card = {
     'question': '',
     'answer': '',
-    'show_answer': False,
-    'show_timer': False,
-    'secs_to_show': False,
+    'show_answer': False, # default do not show answer. clicking True shows the answer with the question.
+    'show_timer': False, # default do not show timer thus unlimited time for practising,vice versa.
+    'secs_to_show': 15, # 15 secs for the timer to end. links to the show_timer
     'score': 0.0 # percentage * 0.7 + (1.0 - secs_to_show * 0.05) * 0.3
 }
-flash_card_list = [flash_card.copy(), flash_card.copy(), flash_card.copy()]
-flash_card_list[-1]['question'] = 'why everything change?'
-for c in flash_card_list:
-    print(c)
-    print(id(c))
+flash_card_list = []
+#
+with open(file_path, 'r') as file:
+    flash_card_list = json.load(file)
+#
+while True:
+    #os.system('clear')
+    current_card = random.choice(flash_card_list)
+    print(current_card['question'])
+    user_answer = input('your answer: ')
+    difference = error_of_tolerance(current_card['answer'], user_answer)
+    print(difference)
+    if user_answer == 'exit':
+        break
+    if user_answer == current_card['answer']:
+        print("absolutely correct!")
+    elif error_of_tolerance(current_card['answer'], user_answer) <= 5:
+        print('almost 100%')
+    else:
+        print('wrong!')
+
+with open(file_path, 'w') as file:
+    json.dump(obj = flash_card_list, fp = file, indent=4)
